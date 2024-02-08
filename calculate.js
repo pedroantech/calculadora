bd665704db44106497eb98803be56b43031a4e86
pedroantech <138733454+pedroantech@users.noreply.github.com>
2024-02-08 11:28:46 -0300

    calculadora

--- calculate.js
+++ calculate.js
@@ -3,8 +3,8 @@ export default function calculate() {
     resultInput.value = "ERROR"
     resultInput.classList.add("error")
     const result = eval(input.value)
-    resultInput.value = result
     
+    resultInput.value = result
     if( result == undefined) {
         resultInput.value = ""
     }
